bc56908106077463fd6f4b3a79cff3d9d338c926
ALAM-97 <alexamara97@gmail.com>
2021-11-09 10:43:19 +0100

    Some Style Added

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,16 +5,19 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Index</title>
+    <link rel="stylesheet" href="{{asset('css/app.css')}}">
 </head>
 <body>
     <h1>I NOSTRI FILM</h1>
     <ul>
         @foreach ($movies as $movie)
             <li>
-                <h2>{{$movie['title']}}</h2>
-                <h4>{{$movie['original_title']}}</h4>
-                <div>Anno di uscita: {{$movie['date']}}</div>
-                <div>Voto: {{$movie['vote']}}</div>
+                <div class="card">
+                    <h2>{{$movie['title']}}</h2>
+                    <h4>{{$movie['original_title']}}</h4>
+                    <div>Anno di uscita: {{$movie['date']}}</div>
+                    <div>Voto: {{$movie['vote']}}</div>
+                    </div>
             </li>
         @endforeach
     </ul>
